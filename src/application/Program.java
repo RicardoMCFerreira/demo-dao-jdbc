dcafaca828e46186f6dcf524c94d60deca848582
RicardoMCFerreira <manuelriky@hotmail.com>
2023-03-10 11:02:23 +0000

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,35 +12,45 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println("=== Test 1: Seller findById ===");
+		System.out.println("===== Test 1: Seller findById =====");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
-		System.out.println("\n=== Test 2: Seller findByDepartment ===");
+		System.out.println("\n===== Test 2: Seller findByDepartment =====");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findBYDepartment(department);
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== Test 3: Seller findAll ===");
+		System.out.println("\n===== Test 3: Seller findAll =====");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 		
-		/*System.out.println("\n=== Test 4: Seller insert ===");
+		/*System.out.println("\n===== Test 4: Seller insert =====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = " + newSeller.getId());*/
 		
-		System.out.println("\n=== Test 5: Seller update ===");
+		System.out.println("\n===== Test 5: Seller update =====");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Waine");
 		System.out.println("Update completed");
+	
+		System.out.println("\n===== Test 6: Seller delete =====");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete Completed");
+		
+		sc.close();
 	}
 
 }

51174992c9cef4c73198d9fc23216ebd7a6a756b
RicardoMCFerreira <manuelriky@hotmail.com>
2023-03-10 10:53:45 +0000

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,10 +31,15 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== Test 4: Seller insert ===");
+		/*System.out.println("\n=== Test 4: Seller insert ===");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
-		System.out.println("Inserted! New Id = " + newSeller.getId());
+		System.out.println("Inserted! New Id = " + newSeller.getId());*/
+		
+		System.out.println("\n=== Test 5: Seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		System.out.println("Update completed");
 	}
 
 }
